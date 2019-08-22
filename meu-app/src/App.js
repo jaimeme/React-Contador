68fbdba5e7f69209fd3ed0ad8035d0d694edc2ca
Jaime Ribeiro <36283050+jaimeme@users.noreply.github.com>
2019-08-22 10:43:17 -0300

    Add files via upload

---
+++ meu-app/src/App.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+import TextCounter from './TextCounter';  
+
+class App extends Component {
+  render(){
+    return (
+      <div>
+        <TextCounter></TextCounter>
+      </div>
+    );
+  }
+}
+
+export default App;
